171a1a6bca673324fb3f00a491525e85343d2301
SadikRa <sadikrahman494@gmail.com>
2023-06-08 10:28:40 +0600

    user post done

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const app = express()
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
-
-
-
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ywq3nhp.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,16 +16,35 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db("Captured_Moments_Institute").collection("users");
+    
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -40,14 +52,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Captured Moments Institute is running");
+});
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('Captured Moments Institute is running')
-  })
-  
 app.listen(port, () => {
-    console.log(`Captured Moments Institute on port ${port}`);
-  })
+  console.log(`Captured Moments Institute on port ${port}`);
+});
